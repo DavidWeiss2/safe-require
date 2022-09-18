d651c12661f18e71c06a68520096d43f4b7f88d3
David Weiss <DavidWeiss2@gmail.com>
2022-09-18 09:49:35 +0300

    simplify implementation

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const fs = require("fs");
 const path = require("path");
-const node_modulesPath = require("node_modules-path");
 
-const safeRequire = (pkg) => {
-  try {
-    return require(pkg);
-  } catch (err) {
-    const pkgDirPath = path.join(node_modulesPath(), pkg);
-    const file = fs.readFileSync(path.join(pkgDirPath, "package.json"));
-    const data = JSON.parse(file);
-    const pkgIndexJsPath = path.join(pkgDirPath, data.main);
-    return require(pkgIndexJsPath);
-  }
+const safeRequire = () => {
+  module.constructor.prototype.require = function (id) {
+    try {
+      this.constructor._load(id, this);
+    } catch (originalError) {
+      const pkgJson = this.constructor._load(
+        path.join(id, "package.json"),
+        this
+      );
+      try {
+        this.constructor._load(path.join(id, pkgJson.main), this);
+      } catch (e) {
+        throw originalError;
+      }
+    }
+  };
 };
+safeRequire();
 module.exports = safeRequire;

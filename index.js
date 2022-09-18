7537f32f64abf1a92e52fff67f52a77bbb808f74
David Weiss <DavidWeiss2@gmail.com>
2022-09-18 10:23:55 +0300

    added return

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const path = require("path");
 const safeRequire = () => {
   module.constructor.prototype.require = function (id) {
     try {
-      this.constructor._load(id, this);
+      return this.constructor._load(id, this);
     } catch (originalError) {
       const pkgJson = this.constructor._load(
         path.join(id, "package.json"),
         this
       );
       try {
-        this.constructor._load(path.join(id, pkgJson.main), this);
+        return this.constructor._load(path.join(id, pkgJson.main), this);
       } catch (e) {
         throw originalError;
       }

082ba96484adf891d3f8509ca491ac68e7abb7a6
David Weiss <DavidWeiss2@gmail.com>
2022-09-18 11:47:07 +0300

    insert pkgjson assginment into trycatch block

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const safeRequire = () => {
     try {
       return this.constructor._load(id, this);
     } catch (originalError) {
-      const pkgJson = this.constructor._load(
-        path.join(id, "package.json"),
-        this
-      );
       try {
+        const pkgJson = this.constructor._load(
+          path.join(id, "package.json"),
+          this
+        );
         return this.constructor._load(path.join(id, pkgJson.main), this);
       } catch (e) {
         throw originalError;
